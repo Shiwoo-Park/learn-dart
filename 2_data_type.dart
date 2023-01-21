2b9dce7ab1213206581bcf6bcb26698cb4e33cac
SHIWOO PARK <shiwoopark@SHIWOOui-noteubug.local>
2023-01-21 10:34:18 +0900

    update

--- 2_data_type.dart
+++ 2_data_type.dart
@@ -1,1 +1,62 @@
-void main() {}
+void main() {
+  String name = "nico";
+  bool isMale = true;
+  int age = 33;
+  double rating = 3.4;
+
+  // int 와 double 은 num 을 상속받는다.
+  num a = 111;
+  a = 1.234;
+
+  // List
+  var giveMeFive = true;
+  var nums = [1, 2, 3, 4];
+  List<int> typedNums = [
+    1,
+    2,
+    3,
+    if (giveMeFive) 5, // element 자리에 expression 삽입 가능
+  ];
+  nums.add(6);
+  print(nums.toString());
+  print(typedNums.toString());
+
+  // String interpolation
+  var animal = 'zebra';
+  var color = "black & white";
+  var animalCount = 1;
+  var sentence = "The color of $animal is $color (count=${animalCount + 2})";
+  print(sentence);
+
+  // Collection + For
+  var oldFriends = ['aaa', 'bbb'];
+  var newFriends = [
+    'ccc',
+    'ddd',
+    for (var friend in oldFriends) "💩${friend}",
+  ];
+  print(newFriends);
+
+  // Map
+  var playerMap = {'name': 'silva', 'xp': 12.22, 'genius': true};
+  print(playerMap['name']);
+
+  List<Map<String, Object>> players = [
+    {'name': 'marry', 'xp': 99.9, 'genius': false}
+  ];
+  players.add(playerMap);
+
+  Map<int, bool> intBoolMap = {1: true, 2: false};
+  Map<List<int>, bool> intListBoolMap = {
+    [1, 1]: true,
+    [1, 2]: false
+  };
+  var tmpKey = [1, 2];
+  print(intListBoolMap[tmpKey]); // null.....?
+
+  // Set
+  var mySet = {1, 2, 3, 4};
+  Set<int> typedSet = {5, 6, 7, 8};
+  mySet.add(3);
+  print(mySet);
+}
--- docs/2_data_type.md
+++ docs/2_data_type.md
@@ -1,2 +1,36 @@
+[prev](../README.md)
+
 # Data types
 
+## Contents
+
+- 모든 타입은 Object 이다. (class)
+- 리스트 타입의 마지막 element 에 쉼표를 찍어주면 여러줄로 자동 포맷팅 해준다.
+- 문자열에 Single & Double quote 둘다 사용 가능
+- 문자열에 변수 집어넣기: `$myVar` 또는 `${myIntVar + 2}`
+- 리스트 안에 element 로 expression 을 집어넣을 수 있음. (if, for 등)
+- Set 은 item 의 유일함(unique) 을 보장
+
+## Code Examples
+
+```dart
+// String interpolation
+var animal = 'zebra';
+var animalCount = 1;
+var sentence = "The count of $animal is ${animalCount + 2}";
+
+// List
+bool giveMeFive = true;
+List<int> typedNums = [
+    1,
+    2,
+    if (giveMeFive) 5, // element 자리에 expression 삽입 가능
+];
+
+
+// Map
+Map<String, Object> playerMap = {'name': 'silva', 'xp': 12.22, 'genius': true};
+
+// Set
+Set<int> mySet = {1,2};
+```
